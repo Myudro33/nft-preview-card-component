15689b3927c9b004866495bb6d2f8c3ff56e84dc
Myudro33 <qanashvili.myudro@gmail.com>
2022-10-05 00:23:05 +0400

    :)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React,{useState} from "react";
+import "./App.css";
+import img from "./assets/image-equilibrium.jpg";
+import eth from "./assets/icon-ethereum.svg";
+import clock from "./assets/icon-clock.svg";
+import avatar from "./assets/image-avatar.png";
+import eye from "./assets/icon-view.svg";
+const App = () => {
+  const [bool,setBool] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="w-full h-screen bg-[#0D192C] flex justify-center items-center">
+      <div className="md:w-[300px] xs:w-[90%] md:h-[510px]  xs:h-[66%] bg-[#15263f] rounded-xl p-5 flex flex-col items-center">
+        <img alt="img" className="rounded-md z-0" src={img} />
+        <span onMouseEnter={()=>{setBool(true)}} onMouseLeave={()=>{setBool(false)}} className="absolute bg-[#00FFF8] md:w-[260px] rounded-md opacity-0 hover:opacity-60 md:h-[260px] duration-150">
+        </span>
+         {bool&&( <img alt="img" className="absolute translate-y-28" src={eye} />)}
+        <p className="text-white font-[outfit] font-semibold text-lg mt-4 w-full hover:text-[#00FFF8]">
+          Equilibrium #3429
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className="text-[#8BACD9] font-[outfit] font-light mt-2">
+          Our Equilibrium collection promotes balance and calm.
+        </p>
+        <div className="flex w-full justify-between mt-4">
+          <p className="text-[#00FFF8] font-[outfit] items-center font-semibold flex">
+            <img alt="img" className="mr-2 h-5" src={eth} />
+            0.041 ETH
+          </p>
+          <p className="text-[#8BACD9] flex items-center font-normal font-[outfit]">
+            {" "}
+            <img alt="img" className="mx-2 h-4 w-4" src={clock} />3 days left
+          </p>
+        </div>
+        <hr className=" w-full border-spacing-1 border-[#2E405A] mt-4" />
+        <div className="w-full flex items-center mt-3">
+          <img
+          alt="img"
+            src={avatar}
+            className="border-2 rounded-full border-[#ffffff] w-8 "
+          />
+          <p className="text-[#8BACD9] font-[outfit] font-normal ml-3">
+            Creation of
+          </p>{" "}
+          <p className="text-white font-[outfit] font-normal ml-2 hover:text-[#00FFF8]">
+            Nika Qanashvili
+          </p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
